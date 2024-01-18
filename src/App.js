ba93bdeca95d7b60dfecbd1570112bac20cb2eba
ginfodata <156537285+ginfodata@users.noreply.github.com>
2024-01-18 15:52:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+   
+   <>
+   <div>
+    <Header></Header>
+    <NavBar></NavBar>
+   </div>
+   
+
+   </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './src/App.css';
+
+export default function Header() {
+  return (
+    <>
+    <div class="header">
+    <h1>Infornix</h1>
+    <p>Your Tagline Goes Here</p>
+  </div>
+
+  <div class="container">
+    <section>
+      <h2>About Us</h2>
+      <p>Welcome to Infornix, where we strive to provide innovative solutions...</p>
+    </section>
+
+    <section>
+      <h2>Services</h2>
+      <p>Explore our cutting-edge services that cater to your business needs...</p>
+    </section>
+  </div>
+    </>
+  )
+}
